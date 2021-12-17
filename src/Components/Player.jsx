f84f417f334cb023e7a0239df9241f1978e8d054
Akash Vaghela <akashvaghela09@gmail.com>
2021-12-18 03:02:54 +0530

    seek, volume changes, fade effect, axios added

--- src/Components/Player.jsx
+++ src/Components/Player.jsx
@@ -1,33 +1,78 @@
 import React, { useEffect, useRef, useState } from 'react';
 import styles from "../Styles/Player.module.css";
-import ReactPlayer from 'react-player/youtube';
-
+import ReactPlayer from 'react-player';
+import data from "../Config/list.json"
 
 
 const Player = () => {
-    const URL = "https://cloud-object-storage-20-bucket-01.s3.jp-tok.cloud-object-storage.appdomain.cloud/Ek%20Passe%20Tu%20Babbu%20-%20Sakhiyaan.mp3";
+    // const URL = "https://cloud-object-storage-20-bucket-01.s3.jp-tok.cloud-object-storage.appdomain.cloud/Ek%20Passe%20Tu%20Babbu%20-%20Sakhiyaan.mp3";
     
-    const [isPlayed, setPlayed] = useState(false)
-    const [volumeValue, setVolumeValue] = useState(1)
-    const [currentTime, setCurrentTime] = useState(0)
-    const [remainingTime, setRemainingTime] = useState(0)
+    const youtubeUrls = [
+        "https://www.youtube.com/watch?v=saYfjjUQ6xw",
+        "https://www.youtube.com/watch?v=8pfdxZAvKoU",
+        "https://www.youtube.com/watch?v=xitd9mEZIHk",
+        "https://www.youtube.com/watch?v=x-KbnJ9fvJc",
+        "https://www.youtube.com/watch?v=5qap5aO4i9A",
+    ]
+    const [isPlayed, setPlayed] = useState(false);
+    const [volumeValue, setVolumeValue] = useState(1);
+    const [currentProgress, setCurrentProgress] = useState(0);
+    const [loadedProgress, setLoadedProgress] = useState(0);
+    const [remainingTime, setRemainingTime] = useState(0);
+    const [totalTime, setTotalTime] = useState(0);
+    const [seekValue, setSeekValue] = useState(0);
     const audioRef = useRef();
     
-    const playFunc = () => {
-        // if(isPlayed == true){
-        //     audioRef.current.play()
-        // } else {
-        //     audioRef.current.pause()
-        // }
-        setPlayed(!isPlayed)
+    const handlePlay = () => {
+        if(isPlayed === true){
+            let count = 1;
+            let timeId = setInterval(() => {
+                count = (count) - 0.1
+                if(count >= 0){
+                    setVolumeValue(count)
+                } else {
+                    setVolumeValue(0)
+                    clearInterval(timeId)
+                    setPlayed(!isPlayed)
+                }
+            }, 150);
+        } else {
+            setPlayed(!isPlayed)
+            let count = 0;
+            let timeId = setInterval(() => {
+                count = (count) + 0.1
+                if(count <= 1){
+                    setVolumeValue(count)
+                } else {
+                    setVolumeValue(1)
+                    clearInterval(timeId)
+                }
+            }, 150);
+        }
     }
     
     const handleVolumeChange = (e) => {
         let tempValue = e.target.value
-        audioRef.current.volume = tempValue / 100;
         setVolumeValue(tempValue)
-        console.log(tempValue);
     }
+    
+    const handleProgress = (e) => {
+        let tempLoad = e.loaded * 100
+        let tempPlayed = e.played * 100
+        setCurrentProgress(tempPlayed+"%")
+        setLoadedProgress(tempLoad+"%")
+        setSeekValue(e.played)
+        
+    }
+    
+    const handleSeekChange = (e) => {
+        let tempSeek = e.target.value
+        audioRef.current.seekTo(tempSeek, "fraction")
+    }
+    
+    useEffect(() => {
+        console.log(data)
+    }, []);
    
     return (
         <div className={styles.wrapper}>
@@ -36,17 +81,20 @@ const Player = () => {
                 playing={isPlayed}
                 volume={volumeValue}
                 controls={true}
+                onProgress={(e) => handleProgress(e)}
+                onDuration={(e) => setTotalTime(e)}
+                ref={audioRef}
+                loop={true}
             />
-            <audio ref={audioRef} src={URL} type="audio/mpeg"></audio>
-            <button className={styles.playButton} onClick={() => playFunc()}>{isPlayed !== true ? "Play" : "Pause"}</button>
+            <button className={styles.playButton} onClick={() => handlePlay()}>{isPlayed !== true ? "Play" : "Pause"}</button>
             <div className={styles.volumeDiv}>
                 <input value={volumeValue} onChange={(e) => handleVolumeChange(e)} type="range" min="0" max="1" step="0.001"/>
                 <h3>Volume {Math.round(volumeValue * 100)}</h3>
             </div>
-            <div className="">
-                <h5>{currentTime}</h5>
-                <input value={currentTime} type="range" />
-                <h5>{remainingTime}</h5>
+            <div className={styles.progressDiv} >
+                <div className={styles.progressLoad} style={{width: loadedProgress}}/>
+                <div className={styles.progressBarVisible} style={{width: currentProgress}}/>
+                <input className={styles.progressBar} value={seekValue} onChange={(e) => handleSeekChange(e)} type="range" min="0" max="1" step="0.001"/>
             </div>
         </div>
     )
