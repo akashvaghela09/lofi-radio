3771b9c92bd5f2083dea1986d36dec8c8e15dab2
Akash Vaghela <akashvaghela09@gmail.com>
2021-12-20 03:23:01 +0530

    radio cards added

--- src/Routes/Radio.jsx
+++ src/Routes/Radio.jsx
@@ -1,12 +1,23 @@
 import React from 'react';
 import { Header } from '../Components/Header';
 import styles from '../Styles/Radio.module.css'
+import radioData from "../Template/radio.json";
 
 const Radio = () => {
     return (
         <div className={styles.wrapper}>
             <Header />
-            
+            <div className={styles.cardSection}>
+                {
+                    radioData.list.map((item) => {
+                        return <div className={styles.card}>
+                        <img src={item.thumbnails.medium.url} alt="radio station" className={styles.cardCover}/>
+                        <h3 className={styles.cardTitle}>{item.title}</h3>
+                        <p className={styles.cardChannelTitle}>{item.channelTitle}</p>
+                        </div>
+                    })
+                }
+            </div>
         </div>
     )
 }
