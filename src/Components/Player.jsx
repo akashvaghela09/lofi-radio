cbb39f6616de7a25c3160a32e5ee5e859e033628
Akash Vaghela <akashvaghela09@gmail.com>
2021-12-20 22:41:41 +0530

    player play and pause functionality added with ui update

--- src/Components/Player.jsx
+++ src/Components/Player.jsx
@@ -1,13 +1,21 @@
 import React, { useEffect, useRef, useState } from 'react';
 import styles from "../Styles/Player.module.css";
 import ReactPlayer from 'react-player';
-import playlistData from "../Template/playlist.json";
-import radioData from "../Template/radio.json";
 import { useDispatch, useSelector } from 'react-redux';
 import { setLoadProgress, setPlayProgress, setPlayStatus, setSeekValue, setTotalPlaytime, setVolumeValue } from '../Redux/player/actions';
+import { BsPlayFill, BsPauseFill } from "react-icons/bs";
+import { BiShuffle } from "react-icons/bi";
+import { MdRepeatOne, MdRepeat } from "react-icons/md";
+import { RiPlayListFill } from "react-icons/ri";
+import { IoVolumeMedium } from "react-icons/io5";
+import { CgPlayTrackNext, CgPlayTrackPrev } from "react-icons/cg";
 
 const Player = () => {
-    const dispatch = useDispatch()
+    const dispatch = useDispatch();
+    const location = window.location.pathname;
+    const [playerVisible, setPlayerVisible] = useState(false);
+    const [playerBarPlayStatus, setPlayerBarPlayStatus] = useState(false);
+    const [playerBarURL, setPlayerBarURL] = useState("")
     const {
         play_status,
         volume_value,
@@ -20,7 +28,8 @@ const Player = () => {
         playlist,
         radiolist,
         play_mode,
-        template_use_status
+        template_use_status,
+        urlList
     } = useSelector((state) => state.player)
     
     const youtubeUrls = [
@@ -35,6 +44,7 @@ const Player = () => {
     
     const handlePlay = () => {
         if(play_status === true){
+            setPlayerBarPlayStatus(false)
             let count = 1;
             let timeId = setInterval(() => {
                 count = (count) - 0.1
@@ -47,6 +57,7 @@ const Player = () => {
                 }
             }, 150);
         } else {
+            setPlayerBarPlayStatus(true)
             dispatch(setPlayStatus(!play_status))
             let count = 0;
             let timeId = setInterval(() => {
@@ -69,9 +80,10 @@ const Player = () => {
     const handleProgress = (e) => {
         let tempLoad = e.loaded * 100
         let tempPlayed = e.played * 100
+        let tempSeek = e.played
         dispatch(setPlayProgress(tempPlayed+"%"))
         dispatch(setLoadProgress(tempLoad+"%"))
-        dispatch(setSeekValue(tempPlayed+"%"))
+        dispatch(setSeekValue(tempSeek))
     }
     
     const handleSeekChange = (e) => {
@@ -79,15 +91,26 @@ const Player = () => {
         audioRef.current.seekTo(tempSeek, "fraction")
     }
     
+ 
     useEffect(() => {
-        console.log(playlistData);
-        console.log(radioData);
+        
+        switch (location) {
+            case "/":
+            case "/radio":
+            case "/playlist":
+                setPlayerVisible(true)   
+                break;
+            default:
+            setPlayerVisible(false)
+                break;
+        }
+        
     }, []);
-    
-    return (
-        <div className={styles.wrapper}>
+                    
+        return (
+        <div className={playerVisible === true ? styles.wrapper : styles.hide}>
             <ReactPlayer 
-                url="https://www.youtube.com/watch?v=NwdQx2P_ytk"
+                url={urlList}
                 playing={play_status}
                 volume={volume_value}
                 controls={true}
@@ -95,18 +118,45 @@ const Player = () => {
                 onDuration={(e) => dispatch(setTotalPlaytime(e))}
                 ref={audioRef}
                 loop={true}
-                // width="0px"
-                // height="0px"
+                width="0px"
+                height="0px"
             />
-            <button className={styles.playButton} onClick={() => handlePlay()}>{play_status !== true ? "Play" : "Pause"}</button>
-            <div className={styles.volumeDiv}>
-                <input value={volume_value} onChange={(e) => handleVolumeChange(e)} type="range" min="0" max="1" step="0.001"/>
-                <h3>Volume {Math.round(volume_value * 100)}</h3>
-            </div>
             <div className={styles.progressDiv} >
                 <div className={styles.progressLoad} style={{width: load_progress}}/>
                 <div className={styles.progressBarVisible} style={{width: play_progress}}/>
                 <input className={styles.progressBar} value={seek_value} onChange={(e) => handleSeekChange(e)} type="range" min="0" max="1" step="0.001"/>
+            </div>
+            <div className={styles.container}>
+                <div className={styles.playerBarCoverSection}>
+                    <div className={styles.playeyBarCoverDiv}>
+                        <img className={styles.playerBarCover} src={play_item.thumbnails.medium.url} alt="stream cover" />
+                    </div>
+                    <div className={styles.playerBarCoverSectionData}>
+                        <p className={styles.playerBarCoverTitle}>{play_item.title}</p>
+                        <p className={styles.playerBarCoverChannel}>{play_item.channelTitle}</p>
+                    </div>
+                </div>
+                <div className={styles.playerBarControlSection}>
+                    {/* <MdRepeatOne className={styles.playerBarRepeatOneIcon}/> */}
+                    {/* <MdRepeat className={styles.playerBarRepeatIcon}/> */}
+                    <CgPlayTrackPrev className={styles.playerBarPrevIcon}/>
+                    {
+                        playerBarPlayStatus === true ? 
+                        <BsPauseFill onClick={() => handlePlay()} className={styles.playerBarPauseIcon}/>
+                        :
+                        <BsPlayFill onClick={() => handlePlay()} className={styles.playerBarPlayIcon}/>
+                    }
+                    <CgPlayTrackNext className={styles.playerBarNextIcon}/>
+                    {/* <BiShuffle className={styles.playerBarShuffleIcon}/> */}
+                </div>
+                <div className={styles.playerBarQueueSection}>
+                    <IoVolumeMedium className={styles.playerBarVolumeIcon}/>
+                    <RiPlayListFill className={styles.playerBarQueueIcon}/>
+                    {/* <div className={styles.volumeDiv}>
+                        <input value={volume_value} onChange={(e) => handleVolumeChange(e)} type="range" min="0" max="1" step="0.001"/>
+                        <h3>Volume {Math.round(volume_value * 100)}</h3>
+                    </div> */}
+                </div>
             </div>
         </div>
     )
