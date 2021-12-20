ae8ab2d13c6cc77bd3db0c6d81ed14f25a6bcf96
Akash Vaghela <akashvaghela09@gmail.com>
2021-12-20 22:40:51 +0530

    Data retrive methods updated

--- src/Routes/Radio.jsx
+++ src/Routes/Radio.jsx
@@ -1,17 +1,33 @@
 import React from 'react';
 import { Header } from '../Components/Header';
 import styles from '../Styles/Radio.module.css'
-import radioData from "../Template/radio.json";
 import { BsPlayFill } from "react-icons/bs";
 import { FiRadio } from "react-icons/fi";
+import { useDispatch, useSelector } from 'react-redux';
 
 const Radio = () => {
+    
+    const {
+        play_status,
+        volume_value,
+        play_progress,
+        load_progress,
+        seek_value,
+        total_playtime,
+        remaining_playtime,
+        play_item,
+        playlist,
+        radiolist,
+        play_mode,
+        template_use_status
+    } = useSelector((state) => state.player)
+    
     return (
         <div className={styles.wrapper}>
             <Header />
             <div className={styles.cardSection}>
                 {
-                    radioData.list.map((item) => {
+                    radiolist.map((item) => {
                         return <div className={styles.card}>
                             <img src={item.thumbnails.medium.url} alt="radio station" className={styles.cardCover}/>
                             <h3 className={styles.cardTitle}>{item.title}</h3>
